e9981101b41f4e77cf1d7dc2272eec4d8c6fe6e3
David <st0724@gmail.com>
2024-08-24 21:12:40 -0700

    Update TextureNativeUtil.cs

--- ImageProcessing/TextureNativeUtil.cs
+++ ImageProcessing/TextureNativeUtil.cs
@@ -6,6 +6,44 @@ using UnityEngine;
 namespace Spark2D {
     public static class TextureNativeUtil {
         [BurstCompile]
+        public struct ColorFillJob : IJobParallelFor {
+            public NativeArray<Color> data;
+            public Color color;
+
+            public static JobHandle Fill(Texture2D texture, Color color, int innerLoopBatchSize = 64, JobHandle dependsOn = default) {
+                var data = texture.GetRawTextureData<Color>();
+                var job = new ColorFillJob {
+                    data = data,
+                    color = color,
+                };
+                return job.Schedule(data.Length, innerLoopBatchSize, dependsOn);
+            }
+
+            public void Execute(int index) {
+                data[index] = color;
+            }
+        }
+
+        [BurstCompile]
+        public struct Color32FillJob : IJobParallelFor {
+            public NativeArray<Color32> data;
+            public Color32 color;
+
+            public static JobHandle Fill(Texture2D texture, Color32 color, int innerLoopBatchSize = 64, JobHandle dependsOn = default) {
+                var data = texture.GetRawTextureData<Color32>();
+                var job = new Color32FillJob {
+                    data = data,
+                    color = color,
+                };
+                return job.Schedule(data.Length, innerLoopBatchSize, dependsOn);
+            }
+
+            public void Execute(int index) {
+                data[index] = color;
+            }
+        }
+
+        [BurstCompile]
         public struct ExtractChannelByteFromColor32 : IJobParallelFor {
             [ReadOnly] public NativeArray<Color32> input;
             public NativeArray<byte> output;
